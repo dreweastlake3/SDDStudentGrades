7449cfeb6b25940a94af74cea5811766dedfa2f8
dreweastlake3 <50123674+dreweastlake3@users.noreply.github.com>
2020-11-30 21:06:51 +1100

    Added Executable and Source

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studGrades = new int[] { 49, 60, 79, 46, 58, 75, 94, 44, 67, 86 };
+
+            foreach (int grade in studGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
